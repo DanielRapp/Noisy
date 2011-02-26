55fb50faaf2f55fa1cb430966c60a9b871afc3f2
DanielRapp <danielrappt@gmail.com>
2011-02-26 01:20:14 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Noisy is a jQuery plugin that adds random to a given element.
+
+#### Here's an example:
+    $('body').noisy({
+        noiseParticles: 40000, 
+        tileSize: 200, 
+        backgroundColor: '#EDEBDE', 
+        maxNoiseOpacity: 0.078
+    });
+
+You can try a demo [here](http://jsbin.com/awole4).
---
+++ jquery.noisy.js
@@ -0,0 +1,40 @@
+(function($){
+	// This function adds non-monochromatic noise to the background-color attribute of an element
+	$.fn.noisy = function(options) {
+		return this.each(function() {
+	 		var canvas = document.createElement('canvas')
+	 		,   ctx = canvas.getContext("2d");
+	 		
+	 		var defaults = {
+	 			noiseParticles:		400000,
+	 			tileSize:			200,
+	 			backgroundColor:	'',
+	 			maxNoiseOpacity:	20/255
+	 		};
+	 		options = $.extend(defaults, options);
+	 		
+	 		canvas.width = canvas.height = options.tileSize;
+	 		
+	 		var imgData = ctx.createImageData(canvas.width, canvas.height);
+	 		
+	 		var rand = function(min, max) {
+	 			return Math.floor(Math.random()*(max-min)+min);
+	 		};
+	 		
+	 		// Add black and white pixels at random positions to the canvas
+	 		while (options.noiseParticles--) {
+	 			var x = rand(1, canvas.width)
+	 			,   y = rand(1, canvas.height)
+	 			,   index = (x + y * imgData.width) * 4;
+	 			
+	 			imgData.data[index  ] = rand(0, 255);							// red
+	 			imgData.data[index+1] = rand(0, 255);							// green
+	 			imgData.data[index+2] = rand(0, 255);							// blue
+	 			imgData.data[index+3] = rand(0, 255*options.maxNoiseOpacity);	// alpha
+	 		}
+	 		
+	 		ctx.putImageData(imgData, 0, 0);
+	 		$(this).css('background', 'url(' + canvas.toDataURL('image/png') + ')' + options.backgroundColor);
+		});
+	};
+})(jQuery);
