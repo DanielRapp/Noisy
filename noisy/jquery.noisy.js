d4e81f7f0be6416293221b2b2824e68e0eae0c24
DanielRapp <danielrappt@gmail.com>
2011-03-09 21:42:40 +0100

    Whitespace and variablename changes, not enough changes to merit a .min update

--- noisy/jquery.noisy.js
+++ noisy/jquery.noisy.js
@@ -4,7 +4,10 @@
 		options = $.extend({}, $.fn.noisy.defaults, options);
 		
 		var url, canvas = document.createElement('canvas');
-		if (!!canvas.getContext) {
+		
+		if (!canvas.getContext) { // Use fallback image if canvas isn't supported
+			url = options.fallback;
+		} else {
 			canvas.width = canvas.height = options.size;
 		
 			var ctx = canvas.getContext('2d'),
@@ -12,24 +15,21 @@
 			    numPixels = options.intensity * Math.pow(options.size, 2),
 			    maxAlpha = 255 * options.opacity;
 			    
-			// Add pixels at random positions to the canvas
+			// Add color to random pixels in the canvas
 			while (numPixels--) { // Read about the double bitwise NOT trick here: goo.gl/6DPpt
 				var x = ~~(Math.random()*canvas.width),
 				    y = ~~(Math.random()*canvas.height),
 				    index = (x + y * imgData.width) * 4;
 				
-				var randCol = numPixels % 255;
-				imgData.data[index  ] = randCol;                                               // red
-				imgData.data[index+1] = options.monochrome ? randCol : ~~(Math.random()*255);  // green
-				imgData.data[index+2] = options.monochrome ? randCol : ~~(Math.random()*255);  // blue
-				imgData.data[index+3] = ~~(Math.random()*maxAlpha);                            // alpha
+				var colorChannel = numPixels % 255; // This will look random enough
+				imgData.data[index  ] = colorChannel;                                               // red
+				imgData.data[index+1] = options.monochrome ? colorChannel : ~~(Math.random()*255);  // green
+				imgData.data[index+2] = options.monochrome ? colorChannel : ~~(Math.random()*255);  // blue
+				imgData.data[index+3] = ~~(Math.random()*maxAlpha);                                 // alpha
 			}
 			
 			ctx.putImageData(imgData, 0, 0);
 			url = canvas.toDataURL('image/png');
-		}
-		else {
-			url = options.fallback;
 		}
 		
 		return this.each(function() {
