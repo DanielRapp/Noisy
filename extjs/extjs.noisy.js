9ce2e5d79f1ffb68241bd1fe64d6715b94e96b5a
Daniel Rapp <danielrappt@gmail.com>
2012-10-11 21:15:00 +0300

    Fixed issue #22

--- extjs/extjs.noisy.js
+++ extjs/extjs.noisy.js
@@ -44,7 +44,7 @@ Ext.override(Ext.Element, {
 			
 				var ctx = canvas.getContext('2d'),
 				    imgData = ctx.createImageData(canvas.width, canvas.height),
-				    numPixels = options.intensity * Math.pow(options.size, 2),
+				    numPixels = Math.round( options.intensity * Math.pow(options.size, 2) ),
 				    maxAlpha = 255 * options.opacity;
 				    
 				// Add color to random pixels in the canvas
